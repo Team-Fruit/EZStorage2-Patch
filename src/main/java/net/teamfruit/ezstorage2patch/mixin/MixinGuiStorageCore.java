9a3af449da93c74c56bcc53e7480dc73b37a7cc4
sjcl <bebe@sjcl.me>
2023-07-10 22:20:07 +0900

    Right-clicking on the search box properly unfilters the container

--- src/main/java/net/teamfruit/ezstorage2patch/mixin/MixinGuiStorageCore.java
+++ src/main/java/net/teamfruit/ezstorage2patch/mixin/MixinGuiStorageCore.java
@@ -2,10 +2,13 @@ package net.teamfruit.ezstorage2patch.mixin;
 
 import com.zerofall.ezstorage.gui.client.GuiContainerEZ;
 import com.zerofall.ezstorage.gui.client.GuiStorageCore;
+import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.inventory.Container;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(GuiStorageCore.class)
@@ -15,8 +18,16 @@ public abstract class MixinGuiStorageCore extends GuiContainerEZ {
         super(inventorySlotsIn);
     }
 
+    @Invoker(value = "searchBoxChange", remap = false)
+    protected abstract void invokeSearchBoxChange(String text);
+
     @Inject(method = "drawScreen", at = @At(value = "TAIL"))
     private void injectDrawScreen(int mouseX, int mouseY, float partialTicks, CallbackInfo ci) {
         this.renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Redirect(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiTextField;setText(Ljava/lang/String;)V"))
+    private void redirectMouseClickedSetText(GuiTextField searchField, String textIn) {
+        this.invokeSearchBoxChange(textIn);
     }
 }
