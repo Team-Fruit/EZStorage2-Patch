71097b6f5d52c87e034c8b84dd8f6002187d9a02
sjcl <bebe@sjcl.me>
2023-07-11 23:19:04 +0900

    Keep container items on the crafting grid

--- src/main/java/net/teamfruit/ezstorage2patch/mixin/MixinContainerStorageCoreCrafting.java
+++ src/main/java/net/teamfruit/ezstorage2patch/mixin/MixinContainerStorageCoreCrafting.java
@@ -3,14 +3,23 @@ package net.teamfruit.ezstorage2patch.mixin;
 import com.zerofall.ezstorage.gui.server.ContainerStorageCore;
 import com.zerofall.ezstorage.gui.server.ContainerStorageCoreCrafting;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.ClickType;
 import net.minecraft.inventory.InventoryCrafting;
+import net.minecraft.inventory.Slot;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Invoker;
 import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ContainerStorageCoreCrafting.class)
-public class MixinContainerStorageCoreCrafting extends ContainerStorageCore {
+public abstract class MixinContainerStorageCoreCrafting extends ContainerStorageCore {
+
+    @Shadow(remap = false)
+    private InventoryCrafting craftMatrix;
 
     public MixinContainerStorageCoreCrafting(EntityPlayer player, World world, int x, int y, int z) {
         super(player, world, x, y, z);
@@ -42,4 +51,37 @@ public class MixinContainerStorageCoreCrafting extends ContainerStorageCore {
             itemStack.setCount(itemStack.getCount() - 1);
         }
     }
+
+    @Redirect(method = "tryToPopulateCraftingGrid", at = @At(value = "INVOKE", target = "Lcom/zerofall/ezstorage/gui/server/ContainerStorageCoreCrafting;clearGrid(Lnet/minecraft/entity/player/EntityPlayer;)V", remap = false), remap = false)
+    private void redirectTryToPopulateCraftingGridClearGrid(ContainerStorageCoreCrafting container, EntityPlayer player) {
+        clearGridWithoutContainerItem(player);
+    }
+
+    private void clearGridWithoutContainerItem(EntityPlayer player) {
+        for (int i = 0; i < 9; ++i) {
+            ItemStack stack = this.craftMatrix.getStackInSlot(i);
+            if (!stack.isEmpty() && !stack.getItem().hasContainerItem(stack)) {
+                ItemStack result = this.tileEntity.input(stack);
+                this.craftMatrix.setInventorySlotContents(i, ItemStack.EMPTY);
+                if (!result.isEmpty()) {
+                    player.dropItem(result, false);
+                }
+            }
+        }
+    }
+
+    @Invoker(value = "tryToPopulateCraftingGrid", remap = false)
+    protected abstract void invokeTryToPopulateCraftingGrid(ItemStack[] recipe, EntityPlayer playerIn);
+
+    @Inject(method = "slotClick", at = @At(value = "INVOKE", target = "Lcom/zerofall/ezstorage/gui/server/ContainerStorageCore;slotClick(IILnet/minecraft/inventory/ClickType;Lnet/minecraft/entity/player/EntityPlayer;)Lnet/minecraft/item/ItemStack;", ordinal = 0), locals = LocalCapture.CAPTURE_FAILHARD, cancellable = true)
+    private void injectSlotClick1(int slotId, int dragType, ClickType clickTypeIn, EntityPlayer player, CallbackInfoReturnable<ItemStack> cir, Slot slot1, ItemStack[] recipe) {
+        int heldItemCountBeforeCraft = player.inventory.getItemStack().getCount();
+        ItemStack result = super.slotClick(slotId, dragType, clickTypeIn, player);
+        int heldItemCountAfterCraft = player.inventory.getItemStack().getCount();
+        if (heldItemCountBeforeCraft < heldItemCountAfterCraft) {
+            this.invokeTryToPopulateCraftingGrid(recipe, player);
+        }
+        cir.setReturnValue(result);
+    }
+
 }
